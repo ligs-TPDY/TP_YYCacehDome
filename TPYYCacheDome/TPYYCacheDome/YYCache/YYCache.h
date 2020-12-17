66228ec4adfa5b09415711425f102e555bee68b2
ligs_TPDY <ligs59800@163.com>
2020-12-17 21:00:03 +0800

    feat(cache):新增YYCaceh代码

--- TPYYCacheDome/TPYYCacheDome/YYCache/YYCache.h
+++ TPYYCacheDome/TPYYCacheDome/YYCache/YYCache.h
@@ -30,20 +30,66 @@ FOUNDATION_EXPORT const unsigned char YYCacheVersionString[];
 NS_ASSUME_NONNULL_BEGIN
 
 
+/**
+ `YYCache` is a thread safe key-value cache.
+ 
+ It use `YYMemoryCache` to store objects in a small and fast memory cache,
+ and use `YYDiskCache` to persisting objects to a large and slow disk cache.
+ See `YYMemoryCache` and `YYDiskCache` for more information.
+ */
 @interface YYCache : NSObject
 
+/** The name of the cache, readonly. */
 @property (copy, readonly) NSString *name;
 
+/** The underlying memory cache. see `YYMemoryCache` for more information.*/
 @property (strong, readonly) YYMemoryCache *memoryCache;
 
+/** The underlying disk cache. see `YYDiskCache` for more information.*/
 @property (strong, readonly) YYDiskCache *diskCache;
 
+/**
+ Create a new instance with the specified name.
+ Multiple instances with the same name will make the cache unstable.
+ 
+ @param name  The name of the cache. It will create a dictionary with the name in
+     the app's caches dictionary for disk cache. Once initialized you should not 
+     read and write to this directory.
+ @result A new cache object, or nil if an error occurs.
+ */
 - (nullable instancetype)initWithName:(NSString *)name;
 
+/**
+ Create a new instance with the specified path.
+ Multiple instances with the same name will make the cache unstable.
+ 
+ @param path  Full path of a directory in which the cache will write data.
+     Once initialized you should not read and write to this directory.
+ @result A new cache object, or nil if an error occurs.
+ */
 - (nullable instancetype)initWithPath:(NSString *)path NS_DESIGNATED_INITIALIZER;
 
+/**
+ Convenience Initializers
+ Create a new instance with the specified name.
+ Multiple instances with the same name will make the cache unstable.
+ 
+ @param name  The name of the cache. It will create a dictionary with the name in
+     the app's caches dictionary for disk cache. Once initialized you should not 
+     read and write to this directory.
+ @result A new cache object, or nil if an error occurs.
+ */
 + (nullable instancetype)cacheWithName:(NSString *)name;
 
+/**
+ Convenience Initializers
+ Create a new instance with the specified path.
+ Multiple instances with the same name will make the cache unstable.
+ 
+ @param path  Full path of a directory in which the cache will write data.
+     Once initialized you should not read and write to this directory.
+ @result A new cache object, or nil if an error occurs.
+ */
 + (nullable instancetype)cacheWithPath:(NSString *)path;
 
 - (instancetype)init UNAVAILABLE_ATTRIBUTE;
